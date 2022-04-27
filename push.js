f7bb379c44b63586fb74f5c0496d10bd1886d7c5
Tannistha Biswas <tannisthabiswasofficial@gmail.com>
2022-04-27 19:14:48 +0530

    first

---
+++ push.js
@@ -0,0 +1,6 @@
+var push = require('web-push');
+
+let vapidKeys = {
+    publicKey: 'BM40pYlNnHiT1mRYhj_AXJqmd4TIoG9VCenkLBRy0MK6IUKOv8ApFh04eC4tLM5G16zQ7xpI5jSefu30lZaqCvA',
+    privateKey: 'ztDQyUpntCQVv1jGRKj0TNph4US43Q-7jhsmmg_m-H0'
+  }
